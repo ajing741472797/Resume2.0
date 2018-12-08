9c92a979d51e20941d8131f83ac6d486cdfaad3d
vic <741472797@qq.com>
2018-12-08 12:28:27 +0800

    给对象一个类

--- JS/base/Model.js
+++ JS/base/Model.js
@@ -1,17 +1,17 @@
 window.Model = function(options){
-    let resourseName = options.resourseName
+    let resourceName = options.resourceName
     return {
-        init: function () {
+        init: function(){
             var APP_ID = 'o8vzed3lpO8Nz3uXvbmjSiPA-gzGzoHsz'
             var APP_KEY = '5ml8Rgc8TNp8sjp2AduI0EF0'
             AV.init({ appId: APP_ID, appKey: APP_KEY })
         },
         fetch: function(){ 
-            var query = new AV.Query(resourseName);
+            var query = new AV.Query(resourceName);
             return query.find()
         },
         save: function(object){
-            var X = AV.Object.extend(resourseName);
+            var X = AV.Object.extend(resourceName);
             var x = new X();
             return x.save(object)
     }
